22fc18d27b6ba860c6a7c6c4c9c1aeee4e5c2984
huangguangda <2397923107@qq.com>
2017-12-02 23:38:13 +0800

    死机完结

--- app/src/main/java/cn/edu/gdmec/android/uilistviewtest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/uilistviewtest/MainActivity.java
@@ -2,12 +2,19 @@ package cn.edu.gdmec.android.uilistviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> ( MainActivity.this, R.layout.simple_list_item_1, data );
+        ListView listView = (ListView) findViewById ( R.id.list_view );
+        listView.setAdapter ( adapter );
     }
 }
