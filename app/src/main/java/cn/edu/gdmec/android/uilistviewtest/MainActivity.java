99669eaae019423aea722db2ac99a4699e0ef527
huangguangda <2397923107@qq.com>
2017-12-03 21:38:33 +0800

    已经恢复

--- app/src/main/java/cn/edu/gdmec/android/uilistviewtest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/uilistviewtest/MainActivity.java
@@ -1,20 +1,29 @@
 package cn.edu.gdmec.android.uilistviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango" };
+    private List<Fruit> fruitList = new ArrayList<Fruit> (  );
+    /*private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango" };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> ( MainActivity.this, R.layout.simple_list_item_1, data );
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter ( MainActivity.this, R.layout.fruit_item, fruitList );
         ListView listView = (ListView) findViewById ( R.id.list_view );
         listView.setAdapter ( adapter );
+      /*  ArrayAdapter<String> adapter = new ArrayAdapter<String> ( MainActivity.this, R.layout.simple_list_item_1, data );
+        ListView listView = (ListView) findViewById ( R.id.list_view );
+        listView.setAdapter ( adapter );*/
+    }
+    private void initFruits(){
+
     }
 }
