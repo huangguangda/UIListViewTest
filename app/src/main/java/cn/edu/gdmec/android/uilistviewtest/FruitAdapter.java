b58cbe977a25bca0e249d69582e24c90e73deed7
huangguangda <2397923107@qq.com>
2017-12-04 18:36:00 +0800

    成功了回复，运行列表...

--- app/src/main/java/cn/edu/gdmec/android/uilistviewtest/FruitAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/uilistviewtest/FruitAdapter.java
@@ -23,7 +23,13 @@ public class FruitAdapter extends ArrayAdapter<Fruit>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         Fruit fruit = getItem ( position );
-        View view = LayoutInflater.from ( getContext () ).inflate ( resourceId, null );
+        View view;
+        if (convertView == null){
+            view = LayoutInflater.from ( getContext () ).inflate ( resourceId, null );
+        }else {
+            view = convertView;
+        }
+        /*View view = LayoutInflater.from ( getContext () ).inflate ( resourceId, null );*/
         ImageView fruitImage = (ImageView ) view.findViewById ( R.id.fruit_image );
         TextView fruitName = (TextView) view.findViewById ( R.id.fruit_name );
         fruitImage.setImageResource ( fruit.getImageId () );
